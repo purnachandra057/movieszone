9ff4d419fbb6593cf37ab692d801bca5fe8f1f47
Prakash Bhattarai <lightvelocity888@gmail.com>
2022-06-20 19:36:16 +0545

    Initial Commit - Project Setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# Movies Zone
+A web application created as college progect that allows students to enlist movies of their choice. 
+
+Created By: Prakash Bhattarai, Saroj Poudel, Tilak Khatri
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+    return (
+        <div className="mx-auto mt-4 w-[760px]">
+            <p className="text-2xl text-gray-800">Welcome to Movieszone</p>
+            <p className="inline-block mt-2 border border-indigo-600 rounded-full px-4 py-1 text-sm text-indigo-400 tracking-wide">
+                Our working for the project begins
+            </p>
+        </div>
+    )
+}
